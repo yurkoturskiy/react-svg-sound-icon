5802af6864e902da16127ac83fbfc500e0e3711b
Yurko Turskiy <guandjoy@gmail.com>
2020-03-09 13:50:47 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import SoundIcon from "./SoundIcon";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <SoundIcon />
+  </React.StrictMode>,
+  rootElement
+);
